e36c9e133bc7b0cc47102775d51be9f1f55ead20
Yanilda <synac1@users.noreply.github.com>
2019-09-03 16:58:14 -0400

    Add files via upload

---
+++ MegaMillion.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ * @author Yanilda
+ * description:
+ *
+ */
+
+import java.util.Random;
+//import the Scanner class
+public class MegaMillion {
+	public static void main(String[] args) {
+		Random randGen= new Random();
+		
+		System.out.println(randGen.nextInt(60)+1);
+		
+		//Declare Variable to hold amount of dollar
+		//Declare Variable for cost of ticket
+	
+		//Prompt the user for the amount
+		//   of dollars to bet on
+		
+		//store amount into amount of dollar
+		//   variable
+		
+		//Prompt the user to enter six numbers
+		//   create a variable for each value
+		
+		//Randomly generate six numbers and compare
+	}
+}
